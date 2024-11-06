859229f8f20da9b7e1cdfc2987a099a3182575ee
nick <nick@avalr.com.ua>
2024-11-06 17:26:00 +0200

    update java version to java 17 (license:format)

--- create-tb-sparkplug-docker.sh
+++ create-tb-sparkplug-docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 #
-# Copyright © ${project.inceptionYear}-2023 The Thingsboard Authors
+# Copyright © ${project.inceptionYear}-2024 The Thingsboard Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
